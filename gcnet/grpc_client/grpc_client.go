d953dbd1c23b5260cabd4bdebe34cf155469e832
Cyber <1322816443@qq.com>
2025-04-07 13:26:09 +0800

    adjust code

--- gcnet/grpc_client/grpc_client.go
+++ gcnet/grpc_client/grpc_client.go
@@ -34,9 +34,10 @@ func (s *GrpcClient) Init(ctx context.Context, option ...any) (err error) {
 	}
 
 	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
-	//linkAddr := fmt.Sprintf("passthrough:%s", s.options.listenAddr)
-	linkAddr := fmt.Sprintf("%s", s.options.listenAddr)
+	linkAddr := fmt.Sprintf("passthrough:%s", s.options.listenAddr)
+	//linkAddr := fmt.Sprintf("%s", s.options.listenAddr)
 	s.client, err = grpc.NewClient(linkAddr, credentials)
+	//s.client, err = grpc.NewClient(linkAddr)
 	if err != nil {
 		log.Error("grpc dial err", zap.Error(err))
 		return
